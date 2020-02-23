61dc40ccefa2215d9db06cf46350f18d2302ca8d
Jakob Au <30506552+jakobau@users.noreply.github.com>
2020-02-23 10:49:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var server = express();
 
-server.use(express.static(__dirname + "/public"));
+server.use(express.static(__dirname + "/public/index.html"));
 
 server.listen(process.env.PORT || 8080);
 
